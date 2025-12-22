fa4b6a7fcf4d480c8e1330e9a1bea126fa0be9e4
chirag <sharmachirag242004@gmail.com>
2025-12-22 11:19:47 +0530

    new

--- first.sql
+++ first.sql
@@ -33,3 +33,42 @@ CREATE TABLE EmployeeSalary (
     EmpID  INT,
     Salary DECIMAL(10,2)
 );
+
+
+
+
+
+
+
+
+
+-- inserting data in tables
+INSERT INTO Employees VALUES
+(101, 'Chirag', 'Sharma', 60000, 10, NULL, TO_DATE('01-11-2025','DD-MM-YYYY'), 21, NULL);
+
+INSERT INTO Employees VALUES
+(102, 'Nikita', 'Dangi', 45000, 20, 101, TO_DATE('15-03-2022','DD-MM-YYYY'), 23, NULL);
+
+INSERT INTO Employees VALUES
+(103, 'Chinnu', 'Sharma', 40000, 10, 101, TO_DATE('10-07-2021','DD-MM-YYYY'), 24, NULL);
+
+INSERT INTO Employees VALUES
+(104, 'Himank', 'Lohar', 55000, 30, 101, TO_DATE('20-01-2020','DD-MM-YYYY'), 26, NULL);
+
+INSERT INTO Employees VALUES
+(105, 'Kapil', 'Kalal', 48000, 20, 102, TO_DATE('05-06-2019','DD-MM-YYYY'), 27, NULL);
+
+INSERT INTO Employees VALUES
+(106, 'Rohit', 'Verma', 52000, 30, 104, TO_DATE('18-09-2018','DD-MM-YYYY'), 28, NULL);
+
+INSERT INTO Employees VALUES
+(107, 'Prateek', 'Singh', 47000, 10, 103, TO_DATE('11-12-2020','DD-MM-YYYY'), 25, NULL);
+
+INSERT INTO Employees VALUES
+(108, 'Karan', 'Mehta', 65000, 40, NULL, TO_DATE('01-04-2017','DD-MM-YYYY'), 30, NULL);
+
+INSERT INTO Employees VALUES
+(109, 'Richa', 'Shah', 43000, 20, 105, TO_DATE('23-08-2021','DD-MM-YYYY'), 24, NULL);
+
+INSERT INTO Employees VALUES
+(110, 'Bhumika', 'Joshi', 39000, 40, 108, TO_DATE('14-02-2023','DD-MM-YYYY'), 22, NULL);
