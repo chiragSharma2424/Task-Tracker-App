584774b9bcfdb13bedd36fb97649f42634e063b7
chirag <sharmachirag242004@gmail.com>
2025-12-22 11:03:51 +0530

    new

--- first.sql
+++ first.sql
@@ -10,3 +10,26 @@ CREATE TABLE Employees (
     age           INT,
     middle_name   VARCHAR(50)
 );
+
+
+CREATE TABLE Departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+
+
+CREATE TABLE EmployeeInfo (
+    EmpID     INT PRIMARY KEY,
+    EmpFname  VARCHAR(50),
+    EmpLname  VARCHAR(50),
+    EmpSalary DECIMAL(10,2),
+    EmpDept   VARCHAR(50),
+    Age       INT,
+    HireDate  DATE
+);
+
+
+CREATE TABLE EmployeeSalary (
+    EmpID  INT,
+    Salary DECIMAL(10,2)
+);
